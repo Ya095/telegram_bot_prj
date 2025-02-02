cebadf1ce7f414c6a8b64cb2e758d91d21d7b178
Egor_Yakovlev1 <egor.yakovlev.95@gmail.com>
2025-02-02 14:38:07 +0300

    add pydantic-settings; work with markdown

--- config.py
+++ config.py
@@ -1,3 +1,13 @@
-import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-bot_token = os.environ["bot_token"]
+
+class Settings(BaseSettings):
+    # ищем нужные значения в переменных окружения
+    model_config = SettingsConfigDict(
+        case_sensitive=False,
+    )
+
+    bot_token: str
+
+
+settings = Settings()
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from config import bot_token
+from config import settings
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart, Command
+from aiogram.utils import markdown
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 
 
 dp = Dispatcher()
@@ -10,12 +13,57 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def handle_start(message: types.Message):
-    await message.answer(text=f"Hello, {message.from_user.full_name}!")
+    url = "https://e7.pngegg.com/pngimages/234/79/png-clipart-black-robot-face-illustration-robotics-technology-computer-icons-internet-bot-robotics-humanoid-robot-industrial-robot-thumbnail.png"
+    await message.answer(
+        text=f"{markdown.hide_link(url=url)}Hello, {markdown.hbold(message.from_user.full_name)}!",
+        parse_mode=ParseMode.HTML,
+    )
 
 
 @dp.message(Command("help"))
 async def handle_help(message: types.Message):
-    text = "I'm an echo bot.\nSend me any message!"
+    # сделать жирным шрифт (вариант 1)
+    # text = "I'm an echo bot.\nSend me any message!"
+    # entity_bold = types.MessageEntity(
+    #     type="bold",
+    #     offset=len("I'm an echo bot.\nSend me "),
+    #     length=3,
+    # )
+    # entities = [entity_bold]
+    # await message.answer(
+    #     text=text,
+    #     entities=entities,
+    # )
+
+    # вариант 2
+    # text = "I'm an echo bot\\.\nSend me *any* message\\!"
+    text = markdown.text(
+        markdown.markdown_decoration.quote("I'm an echo bot."),
+        markdown.text(
+            "Send me",
+            markdown.underline("literally"),
+            markdown.bold("any"),
+            markdown.markdown_decoration.quote("message!"),
+        ),
+        sep="\n",
+    )
+    await message.answer(
+        text=text,
+        # parse_mode=ParseMode.MARKDOWN_V2,
+    )
+
+
+@dp.message(Command("code"))
+async def handle_command_code(message: types.Message):
+    text = markdown.text(
+        "Here's Python code:",
+        "",
+        markdown.markdown_decoration.pre_language(
+            "print('Hello World!')",
+            language="python",
+        ),
+        sep="\n",
+    )
     await message.answer(text=text)
 
 
@@ -23,9 +71,19 @@ async def handle_help(message: types.Message):
 async def echo_msg(message: types.Message):
     await message.bot.send_message(
         chat_id=message.chat.id,
-        text="Wait a second..."
+        text="Wait a second...",
+        parse_mode=None,
     )
-    await message.answer(text="Wait pls...")
+    await message.answer(
+        text="Wait pls...",
+        parse_mode=None,
+    )
+    if message.text:
+        await message.answer(
+            text=message.text,
+            entities=message.entities,
+            parse_mode=None,
+        )
 
     try:
         await message.send_copy(chat_id=message.chat.id)
@@ -44,7 +102,10 @@ async def echo_msg(message: types.Message):
 
 
 async def main():
-    bot = Bot(token=bot_token)
+    bot = Bot(
+        token=settings.bot_token,
+        default=DefaultBotProperties(parse_mode=ParseMode.MARKDOWN_V2),
+    )
     logging.basicConfig(level=logging.INFO)
     # проверка на новые события (в бесконечном цикле)
     await dp.start_polling(bot)
